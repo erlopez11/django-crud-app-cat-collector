002fa9f98fa3ccc05c8f1f6fe253bae9800f00bf
erlopez11 <lopezelainy@gmail.com>
2025-04-22 16:35:03 -0500

    wip: add psycopg2-binary

--- catcollector/settings.py
+++ catcollector/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-b4s*0h)p6@q-w%@200i0%x_0+rtxudd7b6)0p!k9bdyd$@u7r='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,8 +75,8 @@ WSGI_APPLICATION = 'catcollector.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'catcollector',
     }
 }
 
