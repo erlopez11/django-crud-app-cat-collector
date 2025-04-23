192482c15d9a6989e318ca2f205a99258e37af77
erlopez11 <lopezelainy@gmail.com>
2025-04-23 16:35:14 -0500

    fix: key

--- catcollector/settings.py
+++ catcollector/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b4s*0h)p6@q-w%@200i0%x_0+rtxudd7b6)0p!k9bdyd$@u7r='
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
