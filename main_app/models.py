0442d7fd4bfe44aa191c8da9324e8d89c378f498
erlopez11 <lopezelainy@gmail.com>
2025-04-23 16:26:39 -0500

    wip:create & register feeding model

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.urls import reverse
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
 
 class Cat(models.Model):
     name = models.CharField(max_length=100)
@@ -8,5 +15,23 @@ class Cat(models.Model):
 
     def __str__(self):
         return self.name #defines what the string representation of the class will be instead of object(...)
+    
+    def get_absolute_url(self):
+        return reverse("cat-detail", kwargs={"cat_id": self.id})
+    
 
+class Feeding(models.Model):
+    date = models.DateField('Feeding Date')
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+    )
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
 
+    def __str__(self):
+        #this will print 'Breakfast, 'Lunch','Dinner'
+        return f'{self.get_meal_display()} on {self.date}' 
+    
+    class Meta:
+        ordering = ['-date']
